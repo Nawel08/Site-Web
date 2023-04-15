7675c3ee5cfaa2b0d955035977bfda88fc0b238f
ZAIT Ines Nawel <116875486+Nawel08@users.noreply.github.com>
2023-04-15 12:50:28 +0200

    acceuil + chatbot en lien avec la base de données

--- acceuil.php
+++ acceuil.php
@@ -27,96 +27,57 @@
     <div class="fonddumain"></div>
 
     <div class="container">
-        
-        <div class="chat-messages">
-            <?php if (isset($_GET['reponse'])): ?>
-                <div class="chat-message">
-                    <strong>Vous :</strong>
-                    <p><?= htmlspecialchars($_GET['question']) ?></p>
-                </div>
-                <div class="chat-message">
-                    <strong>Chatbot :</strong>
-                    <p><?= htmlspecialchars($_GET['reponse']) ?></p>
-                </div>
-            <?php endif; ?>
-        </div>
+        <div class="chat-messages"></div>
     </div>
 
     <div class="chatbot-icon">
-    <img src="chatbotb.png" width="50px" height="50px" alt="Chatbot Icon" id="chatbot-icon">
-</div>
+        <img src="chatbotb.png" width="50px" height="50px" alt="Chatbot Icon" id="chatbot-icon">
+    </div>
 
-
-    <div class="chatbot-container" id="chatbot-container">
-
-        <form action="acceuil.php" method="post">
+    <div class="chatbot-container hidden" id="chatbot-container">
+        <form>
             <input type="text" id="question_2" name="question" placeholder="Posez votre question..." required>
             <button type="submit">Envoyer</button>
         </form>
+        <div class="chat-messages"></div>
+    </div>
 
-        <div class="chat-messages">
-            <?php
-            if (isset($_POST['question'])) {
-                $question = strtolower(trim($_POST['question']));
-                $reponse = '';
-
-                $keywords = [
-    'bonjour' => ['Bonjour ! Comment allez-vous ?', 'Bonjour ! Que puis-je faire pour vous ?'],
-    'ça va' => ['Très bien, merci ! Et vous ?', 'Ça va bien, et vous ?'],
-    'aide' => ['Bien sûr, comment puis-je vous aider ?', 'Je suis là pour vous aider, de quoi avez-vous besoin ?'],
-    'études' => ['Nous proposons des cours en ligne pour les étudiants de tous les niveaux. Découvrez nos offres sur la page "Nos abonnements" !', 'Avec N&T School, vous pouvez étudier depuis chez vous avec des cours en ligne de qualité.'],
-    'tarifs' => ['Nos prix dépendent du type d\'abonnement que vous choisissez. Rendez-vous sur la page "Nos abonnements" pour découvrir nos offres !', 'Les prix de nos abonnements varient en fonction de la durée et du niveau d\'études.'],
-    'connexion' => ['Pour vous connecter, rendez-vous sur la page "Connexion" et entrez vos identifiants.', 'Vous pouvez accéder à votre compte en vous connectant sur la page "Connexion".'],
-];
-
-$question = strtolower($question);
-
-foreach ($keywords as $keyword => $responses) {
-    foreach ($responses as $response) {
-        if (strpos($question, $keyword) !== false) {
-            $reponse = $response;
-            break 2;
+    <script>
+        function toggleChatbot() {
+            var chatbotIcon = document.getElementById('chatbot-icon');
+            var chatbotContainer = document.getElementById('chatbot-container');
+            
+            chatbotContainer.classList.toggle('hidden');
         }
-    }
-}
 
-if ($reponse == '') {
-    $reponse = 'Désolé, je ne comprends pas votre question. Pouvez-vous reformuler s\'il vous plaît ?';
-	}
-			}
-?>
+        document.getElementById('chatbot-icon').addEventListener('click', toggleChatbot);
 
-<div class="chat-messages">
-    <?php if (isset($_POST['question'])): ?>
-        <div class="chat-message">
-            <strong>Vous :</strong>
-            <p><?= htmlspecialchars($question) ?></p>
-        </div>
-        <div class="chat-message">
-            <strong>Chatbot :</strong>
-            <p><?= htmlspecialchars($reponse) ?></p>
-        </div>
-    <?php endif; ?>
-</div>
-<script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var chatbotContainer = document.getElementById('chatbot-container');
+            chatbotContainer.classList.add('hidden');
+        });
 
-    function toggleChatbot() {
-        var chatbotIcon = document.getElementById('chatbot-icon');
-        var chatbotContainer = document.getElementById('chatbot-container');
-        
-        chatbotContainer.classList.toggle('hidden');
-    }
-	document.getElementById('chatbot-icon').addEventListener('click', toggleChatbot);
+        $(document).ready(function() {
+    $("form").on("submit", function(event) {
+        event.preventDefault();
+        var question = $("#question_2").val();
+        var userMessage = '<div class="chat-message"><strong>Vous :</strong><p>' + question + '</p></div>';
+        $(".chat-messages").append(userMessage);
+        $("#question_2").val('');
 
-
-    document.addEventListener('DOMContentLoaded', function() {
-    var chatbotContainer = document.getElementById('chatbot-container');
-    chatbotContainer.classList.add('hidden');
+        // start ajax code
+        $.ajax({
+            url: 'message.php',
+            type: 'POST',
+            data: 'text=' + question,
+            success: function(result) {
+                var botReply = '<div class="chat-message"><strong>Chatbot :</strong><p>' + result + '</p></div>';
+                $(".chat-messages").append(botReply);
+                $(".container .chat-messages").scrollTop($(".container .chat-messages")[0].scrollHeight);
+            }
+        });
+    });
 });
-
 </script>
-
-
-
 </body>
 </html>
