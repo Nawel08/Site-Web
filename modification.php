aa799c58737c1ea3e60fd7d36f163a5dcdd8b450
tesnime94 <130251538+tesnime94@users.noreply.github.com>
2023-04-16 04:55:17 +0200

    Update modification.php

--- modification.php
+++ modification.php
@@ -26,6 +26,32 @@ if(isset($_POST['nvxmdp']) AND !empty($_POST['nvxmdp']) AND isset($_POST['nvxmdp
 		$erreur="Vos deux mots de passes ne correspondent pas";
 	}
 }
+if (isset($_FILES['avatar']) AND !empty($_FILES['avatar']['name'])){ //on vérifie si l'avatar est définis et si le nom de la photo de son avatar existe bien dans le dossier 
+$taillemax= 2097152;//la taille maximum de la photo en octet
+$extensionsValides=array('jpg','jpeg','png','gif'); //on initialise une variable de "verification" pour autorise après l'utilisateur a utilisé uniquement des fichier de ce type
+	if (!$_FILES['avatar']['size']<=$taillemax){ //si la taille de la photo de l'avatar est bien respecté, soit plus petite que la taille maximum que j'ai fixé
+		$extensionUpload=strtolower(substr(strrchr($_FILES['avatar']['name'],'.'),1)); 
+		//strrchr pour renvoyer l'extension du fichier avec le point et le substring va nous permettre d'ignorer le premier caractère de la chaine et le strtolower pour tout mettre en minuscule pas que l'utilisateur mette en majuscule et que comme j'ai mis "png" en minuscule qu'il ne comprenne pas
+		if(in_array($extensionUpload,$extensionsValides)){ //pour vérifier si l'extensionupload est bien dans les valeurs de extensions valides c'st à dire que l'extension de l'image de l'avatar corrrespond bien
+			$chemin="utilisateur/avatar/".$_SESSION['mail'].".".$extensionUpload; //chemin vers lequel notre photo va aller
+			$resultat=move_uploaded_file($_FILES['avatar']['tmp_name'],$chemin); //pour déplacer le fichier de l'avatar, tmp_name est l'endroit ou se situe le fichier temporairement pour qu'on le déplace dans notre variable $chemin
+			if($resultat){ //si le fichier c'est bien importé
+				$updateavatar=$DB->prepare('UPDATE utilisateur SET avatar=:avatar WHERE mail= :mail'); //on prépare la requete sql afin d'ajouter ou modifie en fonction de si il y avait ou non un avatar au mail de l'utilisateur 
+				$updateavatar->execute(array('avatar'=>$_SESSION['mail'].".".$extensionUpload,'mail'=>$_SESSION['mail'])); //on recup le nom du fichier avec avatar de l'amil
+				header ('Location: moncompte.php?mail='.$_SESSION['mail']);
+			}
+			else{ //sinon
+				$erreur="Erreur au cours de l'importation de la photo de profil";
+			}
+		}
+		else{
+			$erreur='Votre photo de profil doit être au format gif, jpeg, jpg ou png';
+		}
+}}
+	else{
+		$erreur="Votre photo de profil ne doit pas dépasser 2Mo";
+	}
+
 	
 ?>
 
@@ -40,18 +66,22 @@ if(isset($_POST['nvxmdp']) AND !empty($_POST['nvxmdp']) AND isset($_POST['nvxmdp
 <?php
 	require('header.html')
 	?>
-	
+	<br><br><br><br>
 	<section class="moncompte">
+	<br><br><br>
 	<h2> Modification de profil</h2>
-	<form method="POST" action=""><table>
+	<form method="POST" action="" enctype="multipart/form-data"><table> <!-- On ajoute un type d'encodage pour que l'avatar fonctionne-->
 	<tr><td><label>Mon prénom :</label></td><td><input type="text" name="nvxprenom" value="<?php echo $user['prenom']; ?>"></td></tr>
 	<tr><td><label>Adresse Mail :</label></td><td><input type="text" name="nvxmail" value="<?php echo $user['mail']; ?>"></td></tr>
 	<tr><td><label>Mot de passe : </label></td><td><input type="password" name="nvxmdp"></td></tr>
 	<tr><td><label>Confirmation du mot de passe: </label></td><td><input type="password" name="nvxmdp2"></td></tr>
-	<tr><td></td><td><input type="submit" value="Mettre à jour mon profil"></td></tr>
+	<tr><td><label>Mon avatar :</label></td><td><input type="file" name="avatar"></td></tr>
+	<tr><td></td></tr>
+	<tr><td></td><td><input type="submit" value="Mettre à jour mon profil" ></td></tr>
 	</table>
 	<?php if (isset($erreur)){echo $erreur;} //si l'erreur existe on l'affiche?>
 	</form>
+	<br><br><br>
 	</section>
 	
 	<br><br><br><br><br><br><br>
@@ -68,4 +98,5 @@ else{
 header("Location: login.php");}
 
 ?>
+
 
