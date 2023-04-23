323f8d8288e82c9f17b5fd0dff76dd4db4dcd077
ZAIT Ines Nawel <116875486+Nawel08@users.noreply.github.com>
2023-04-23 21:34:37 +0200

    Update supprimercompte.php
    commenté + noms

--- supprimercompte.php
+++ supprimercompte.php
@@ -1,15 +1,16 @@
+<!-- BEN OMRANE et ZAIT -->
 <?php
-session_start(); //on démarre la session
-$DB= new PDO('mysql:host=127.0.0.1;dbname=siteweb','root',''); //connexion à la base de donnée
-if(isset($_SESSION['mail'])){//si ma variable de session mail existe
-$getmail=htmlspecialchars($_SESSION['mail']);
-$requser=$DB->prepare('DELETE FROM utilisateur WHERE mail=?');
-$requser->execute(array($getmail)); //execution de la requete avec notre getid on r id que celui du login
-
-if ($requser->rowCount()>0){
-	header('Location:BD_Projet_Inscription.php');
+session_start(); // Démarrage de la session
+$DB= new PDO('mysql:host=127.0.0.1;dbname=siteweb','root',''); // Connexion à la base de données
+if(isset($_SESSION['mail'])){ // Si la variable de session 'mail' existe
+  $getmail=htmlspecialchars($_SESSION['mail']); // Récupération de la variable 'mail' de la session et protection contre les failles XSS
+  $requser=$DB->prepare('DELETE FROM utilisateur WHERE mail=?'); // Préparation de la requête SQL pour supprimer l'utilisateur correspondant au mail
+  $requser->execute(array($getmail)); // Exécution de la requête avec le mail récupéré
+  if ($requser->rowCount()>0){ // Si une ligne a été supprimée dans la table 'utilisateur'
+    header('Location:BD_Projet_Inscription.php'); // Redirection vers la page d'inscription
+  }
+  else{
+    echo("erreur de suppression"); // Affichage d'un message d'erreur si aucune ligne n'a été supprimée
+  }
 }
-else{
-	echo("erreur de suppression");
-	
-}}
+?>
