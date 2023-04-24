686384371b0f4692b78ff6e825cf7ac1b243f51e
ZAIT Ines Nawel <116875486+Nawel08@users.noreply.github.com>
2023-04-24 10:18:13 +0200

    Update moncompte.php

--- moncompte.php
+++ moncompte.php
@@ -1,70 +1,85 @@
 <?php
-session_start(); //on démarre la session
-$DB= new PDO('mysql:host=127.0.0.1;dbname=siteweb','root',''); //connexion à la base de donnée
-if(isset($_SESSION['mail'])){//si ma variable de session mail existe
-$getmail=htmlspecialchars($_SESSION['mail']); //pour sécuriser la variable
-$requser=$DB->prepare('SELECT * FROM utilisateur WHERE mail=?');
-$requser->execute(array($getmail)); //execution de la requete avec notre getid on récup le même id que celui du login
-$userinfo=$requser->fetch(); //on affiche les données de la base de donnée
+session_start();
+$DB = new PDO('mysql:host=127.0.0.1;dbname=siteweb', 'root', '');
+
+if (isset($_SESSION['mail'])) {
+    $getmail = htmlspecialchars($_SESSION['mail']);
+    $requser = $DB->prepare('SELECT * FROM utilisateur WHERE mail=?');
+    $requser->execute(array($getmail));
+    $userinfo = $requser->fetch();
+}
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
-<meta charset="utf-8">
-<title>N&T School | Mon Compte </title>
-<link rel="stylesheet" type="text/css" href="moncompte2.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>N&T School | Mon Compte</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="moncompte.css">
 </head>
 <body>
-<?php
-	require('header.html')
-	?>
-	
-	<section>
-	<br><br><br><br><br><br>
-	<br><br>
-	<div class="moncompte">
-	<h2>| Mon Compte </h2>
-	<br>
-	<h5>Mes informations personelles </h5>
+    <?php if (isset($_SESSION['mail'])): ?>
+        <?php require('header.html') ?>
+<br>
+					<br>
+					<br>
+					<br>
+        <main class="container">
+            <section class="mt-5">
+                <div class="card">
+                    <div class="card-header">
+					<br>
+					<br>
+					
+                        <h2 style=" color: black;">Mon Compte</h2>
+						
+                    </div>
+                    <div class="card-body">
+					<br>
+					<br>
+					
+                        <h5 style="color: black;" >Mes informations personnelles</h5>
+						<br>
+					<br>
+                        <table class="table">
+                            <tr>
+                                <td width="20%">Prénom :</td>
+                                <td><?php echo $userinfo['prenom']; ?></td>
+                                <td></td>
+                                <td style="text-align:center;"><label>Photo de profil :</label></td>
+                            </tr>
+                            <tr>
+                                <td>Email :</td>
+                                <td><?php echo $userinfo['mail']; ?></td>
+                                <td></td>
+                                <td style="text-align: center" rowspan='2'>
+                                    <?php if (!empty($userinfo['avatar'])): ?>
+                                        <img width="20%" height="20%" style="border-radius: 25% 10%;" src="utilisateur/avatar/<?php echo $userinfo['avatar']; ?>">
+                                    <?php endif; ?>
+                                </td>
+                            </tr>
+                        </table>
+                        <div class="mt-4">
+                            <a href="modification.php" class="btn btn-primary">Modifier mon profil</a>
+                            <a href="deconnexion.php" class="btn btn-secondary">Se déconnecter</a>
+                            <a href="supprimercompte.php?mail=<?php echo $userinfo['mail']; ?>" class="btn btn-danger">Supprimer compte</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
 
-	<br><br>
-	<table >
-	<tr><td width="20%">Prénom :</td><td><?php echo $userinfo['prenom']; ?></td><td></td><td style="text-align:center;"><label>Photo de profil :</label></td></tr>
-	<tr><td>Email :</td><td><?php echo $userinfo['mail'];?></td><td></td><td style="text-align: center" rowspan='2'><?php if (!empty($userinfo['avatar'])){ ?><img width="70%" height="70%"  style="border-radius: 25% 10%;" src="utilisateur/avatar/<?php echo $userinfo['avatar'];?>"> <?php } 
-		//si la variable de l'avatar dans la base de donnée n'est pas vide?></td></tr> 
-	<tr><td>Mot de passe :</td><td></td></tr><tr></td><td></td></tr>
-	</table>
-	<br><br>
-	
-	<h5>Mes abonnements </h5>
-	<table>
-	<tr><td>Abonnement en cours :</td><td><?php echo $userinfo['mail'];?></td></tr><tr></td><td></td></tr>
-	</table>
-	<br><br><br><br>
-	
-	<a href="modification.php">Modifier mon profil </a><br><br>
-	<a href="deconnexion.php">Se déconnecter </a><br><br>
-	<a href="supprimercompte.php?mail=<?php $userinfo['mail'];?>">Supprimer compte </a><br><br>
-	
-	<br><br><br>
-	</section>
-	</div>
-	<br><br><br><br><br><br><br><br><br>
-	<?php
-	require('footer.html');
-	?>
-	
-	</body>
+        <?php require('footer.html') ?>
+    <?php else: ?>
+        <?php header("Location: login.php"); ?>
+    <?php endif; ?>
 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
 </html>
-<?php
-}
-else{
-header("Location: login.php");} //si on clique sur compte mais qu'on n'est pas connecté cela nous renvoie vers la page connexion
-
-?>
-
-
 
 
